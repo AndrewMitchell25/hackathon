525f926bdb7e1bc26c3ef18c90431b9100927b18
Jerrick Ban <pokemonesbae@gmail.com>
2022-03-26 13:37:13 -0400

    flask

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from website import create_app
+from website.templates import create_app
+from flask import Flask, render_template, request, redirect, session
+import sqlite3,pandas
 
 app = create_app()
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
