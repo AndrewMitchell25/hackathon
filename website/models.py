aa677e9b0d86512b4c60e655ee70afa95537b56c
Kyle Phan <kylephan@dhcp-10-5-63-59.dhcp.nd.edu>
2022-03-26 19:45:18 -0400

    fixing meb

--- website/models.py
+++ website/models.py
@@ -2,11 +2,13 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(150))
     state = db.Column(db.String(50))
     zip_code = db.Column(db.Integer)
+    meb = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
@@ -16,4 +18,3 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     address = db.relationship('Address')
-
