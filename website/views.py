01bbac864f783cafa949ff4ece15dbbc826424ec
Kyle Phan <kylephan@dhcp-10-5-63-59.dhcp.nd.edu>
2022-03-26 18:59:18 -0400

    added meb

--- website/views.py
+++ website/views.py
@@ -5,26 +5,29 @@ from . import db
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 @login_required
 def home():
 
     return render_template("home.html", user=current_user)
 
+
 @views.route('/location', methods=['GET', 'POST'])
 def location():
     if request.method == 'POST':
         #num_rows_deleted = db.session.query(User).delete()
-        #db.session.commit()
+        # db.session.commit()
         address = request.form.get('address')
         state = request.form.get('state')
         zip_code = request.form.get('zip_code')
+        meb = request.form.get('meb')
 
-        new_address = Address(address=address,state=state,zip_code=int(zip_code), user_id=current_user.id)
+        new_address = Address(address=address, state=state, zip_code=int(
+            zip_code), meb=int(meb), user_id=current_user.id)
         db.session.add(new_address)
         db.session.commit()
         flash('Address added!', category='success')
         return redirect(url_for('views.home'))
 
     return render_template("location.html", user=current_user)
-
