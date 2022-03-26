dfa3ce720e307800b9879aaf7d65c59f3ff8ed9e
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-03-26 16:25:05 -0400

    basic login function working on location

--- website/models.py
+++ website/models.py
@@ -2,10 +2,11 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
+class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    address = db.Column(db.String(150))
+    state = db.Column(db.String(50))
+    zip_code = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
@@ -14,5 +15,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    address = db.relationship('Address')
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, flash, url_for
+from flask_login import login_required, current_user
+from .models import User, Address
+from . import db
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
+
+@views.route('/location', methods=['GET', 'POST'])
+def location():
+    if request.method == 'POST':
+        address = request.form.get('address')
+        state = request.form.get('state')
+        zip_code = request.form.get('zip_code')
+
+        new_address = Address(address=address,state=state,zip_code=int(zip_code), user_id=current_user.id)
+        db.session.add(new_address)
+        db.session.commit()
+        flash('Address added!', category='success')
+        return redirect(url_for('views.home'))
+
+    return render_template("location.html", user=current_user)
